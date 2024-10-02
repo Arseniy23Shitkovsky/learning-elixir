37f73db6f4b9655baf828ec4c3295f4061a547ac
a.shchitkovskiy <a.shchitkovskiy@fun-box.ru>
2024-10-02 12:32:34 +0300

    Добавил серверный процесс сложения двух чисел. Тест на него

--- test/learning_elixir_test.exs
+++ test/learning_elixir_test.exs
@@ -1,8 +1,14 @@
 defmodule LearningElixirTest do
   use ExUnit.Case
   doctest LearningElixir
+  import SumServer
 
   test "greets the world" do
     assert LearningElixir.hello() == :world
+  end
+
+  test "call gen server proccess" do
+    {:ok, server} = start()
+    assert 6 == sum(server, 3, 3)
   end
 end
